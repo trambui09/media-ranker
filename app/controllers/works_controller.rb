3f804b6164d445c3995ae17e9df91b4d9720772a
Tram Bui <pbui17@cmc.edu>
2020-11-08 19:28:29 -0600

    wrote the seeds and index.html.erb files

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,30 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+WORK_FILE = Rails.root.join('db', 'works-seeds.csv')
+puts "Loading raw work data from #{WORK_FILE}"
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
