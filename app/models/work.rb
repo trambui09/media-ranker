f3514fb98fce30a7728b2e915f0b48759457e69e
Tram Bui <pbui17@cmc.edu>
2020-11-09 15:46:48 -0600

    validates the presence of a title

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,7 @@
 class Work < ApplicationRecord
 
+  validates :title, presence: true
+
   def work_category(category)
     works_in_category = Work.where(category: category)
     return works_in_category
