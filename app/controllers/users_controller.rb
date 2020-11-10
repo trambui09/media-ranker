71f784d1be880e191caabcfb58e5a6a3172f4d9f
Tram Bui <pbui17@cmc.edu>
2020-11-10 12:30:53 -0600

    wrote user login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+
+    if user # existing user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
+    else # new user "Successfully created new user testingcat with ID 874"
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+    end
+
+    redirect_to root_path
+    return
   end
 end
