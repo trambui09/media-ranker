3543539d0e0f9f99d7aced0d8624cd486cbc4fba
Tram Bui <pbui17@cmc.edu>
2020-11-10 12:34:56 -0600

    logout and current user actions written

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,22 @@ class UsersController < ApplicationController
     redirect_to root_path
     return
   end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Succesfully logged out"
+    redirect_to root_path
+    return
+  end
+
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+
+    unless @current_user
+      # TODO: must be logged in to upvote
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+      return
+    end
+  end
 end
