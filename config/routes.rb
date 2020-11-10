67d91487071d7eb6184475a5fa39f8e6b08ed813
Tram Bui <pbui17@cmc.edu>
2020-11-10 12:16:12 -0600

    added customer user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/login_form'
-  get 'users/login'
+  get '/login', to: "users#login_form", as: "login"
+  post '/login', to: "users#login"
+  post '/logout', to: "users#logout", as: "logout"
+  get '/users/current', to: "users#current", as: "current_user"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homepages#index'
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,32 @@
 require "test_helper"
 
 describe UsersController do
-  it "must get login_form" do
-    get users_login_form_url
+  it "can get login_form" do
+    get login_path
+
     must_respond_with :success
   end
 
-  it "must get login" do
-    get users_login_url
-    must_respond_with :success
+  describe "logging in" do
+    it "can login a new user" do
+      user_hash = {
+          user: {
+              username: "Testing Username"
+          }
+      }
+
+      expect {
+        post login_path, params: user_hash
+      }.must_differ "User.count", 1
+
+      must_respond_with :redirect
+      user = User.find_by(username: user_hash[:user][:username])
+
+      expect(user).wont_be_nil
+      expect(session[:user_id]).must_equal user.id
+      expect(user.username).must_equal user_hash[:user][:username]
+    end
   end
+
 
 end
