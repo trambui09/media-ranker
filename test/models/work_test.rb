ecb82b5e8f29892ec65f49fadb0821196e373c0f
Tram Bui <pbui17@cmc.edu>
2020-11-09 20:34:00 -0600

    worked on work model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -41,4 +41,47 @@ describe Work do
     end
 
   end
+
+  # describe "relations" do
+  #
+  # end
+
+  describe "custom methods" do
+    # TODO: not sure how I'd test for random?
+    describe "spotlight" do
+      before do
+        @work_1 = Work.create(category: "book",
+                    title: "test title",
+                    creator: "test creator",
+                    publication_year: 2020,
+                    description: "test description"
+        )
+
+        @work_2 = Work.create(category: "book",
+                    title: "test title 2 ",
+                    creator: "test creator 2 ",
+                    publication_year: 2019,
+                    description: "test description 2"
+        )
+      end
+
+      it "returns a random work" do
+        # arrange
+        sample_work = Work.spotlight
+        # act
+        # assert
+        expect(sample_work).must_be_instance_of Work
+        expect(sample_work.title).wont_be_nil
+      end
+    end
+
+    describe "works_in_category" do
+
+    end
+
+    describe "top ten" do
+      
+    end
+
+  end
 end
