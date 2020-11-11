07fbfeed05d249b3c70f0a831e1199054db3a698
Tram Bui <pbui17@cmc.edu>
2020-11-11 17:03:46 -0600

    added validation tests for votes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,21 +1,35 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @user = users(:miso)
+    @work = works(:cowboy)
+  end
+  let (:new_vote) {
+    Vote.new(user_id: @user.id ,
+             work_id: @work.id
+             )
+  }
   describe "validations" do
+
+    it "is valid when all the fields are present" do
+      new_vote.save
+
+      expect(new_vote.valid?).must_equal true
+
+    end
+
+    it "is invalid if the user has already voted for the work" do
+      Vote.create(user_id: @user.id, work_id: @work.id)
+
+      expect(new_vote.valid?).must_equal false
+      expect(new_vote.errors.messages).must_include :work_id
+      expect(new_vote.errors.messages[:work_id].include?("has already been taken")).must_equal true
+
+    end
 
   end
   describe "relations" do
-    # it "has a work" do
-    #
-    #   vote = votes(:first)
-    #
-    #   expect(vote.work_id).must_equal works(:cowboy).id
-    #
-    # end
-
     it "can set the work" do
       vote = Vote.new
       vote.work = works(:cowboy)
@@ -24,9 +38,6 @@ describe Vote do
     end
 
     # TODO: check if I need the "has a work/user" tests
-    it "has a user" do
-
-    end
 
     it "can set the user" do
       vote = Vote.new
