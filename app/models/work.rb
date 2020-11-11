039c1a4755e0ac77815484db8254aa353a15ae1f
Tram Bui <pbui17@cmc.edu>
2020-11-10 18:20:34 -0600

    generated a vote model and set up the relations between all three models

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,9 @@ class Work < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
 
+  has_many :votes
+  has_many :users, through: :votes
+
   def work_category(category)
     works_in_category = Work.where(category: category)
     return works_in_category
