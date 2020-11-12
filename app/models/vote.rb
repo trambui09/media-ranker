31b9b6aedcdfc907c72aea9dfb5943c5ee5f7d59
Tram Bui <pbui17@cmc.edu>
2020-11-12 12:20:55 -0600

    changed vote validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,11 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   # validates :work uniqueness: { scope: :user }
-  validates_uniqueness_of :work_id, scope: :user_id
+  # validates_uniqueness_of :work_id, scope: :user_id
   # validates_uniqueness_of :vote, scope: [:work_id, :user_id]
+
+  validates :user_id, uniqueness: { scope: :work_id,
+                                    message: "has already voted for this work"
+  }
 
 end
