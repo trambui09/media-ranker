2195e7f3fd97db2cd26121713a32b2409be0e22d
Tram Bui <pbui17@cmc.edu>
2020-11-12 18:32:35 -0600

    view partials for homepage top_ten

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,7 +6,7 @@ class Vote < ApplicationRecord
   # validates_uniqueness_of :work_id, scope: :user_id
   # validates_uniqueness_of :vote, scope: [:work_id, :user_id]
 
-  validates :user_id, uniqueness: { scope: :work_id,
+  validates :user, uniqueness: { scope: :work,
                                     message: "has already voted for this work"
   }
 
