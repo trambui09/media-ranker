c462c6f1966672040d5cf76a92b917f00dc0ad5b
Tram Bui <pbui17@cmc.edu>
2020-11-13 11:00:47 -0600

    flash with bootstrap alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,15 @@ module ApplicationHelper
   #   return "[unknown]" unless date
   #   return content_tag(:span, "#{date_voted_on(date)}", class: 'date', title: date.to_s)
   # end
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-warning" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert"))
+    end
+    nil
+  end
 
 end
