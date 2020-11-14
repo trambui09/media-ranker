71e77a8258452ca3278d72a4d42582fecfac6d20
Tram Bui <pbui17@cmc.edu>
2020-11-14 11:48:48 -0600

    user_controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,15 +44,15 @@ class UsersController < ApplicationController
   end
 
   # TODO: move finding a logged in user to the application_controller.rb
-  # def current
-  #   @current_user = User.find_by(id: session[:user_id])
-  #
-  #   unless @current_user
-  #     # TODO: must be logged in to upvote
-  #     flash[:error] = "You must be logged in to see this page"
-  #     redirect_to root_path
-  #     return
-  #   end
-  # end
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+
+    unless @current_user
+      # TODO: must be logged in to upvote
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+      return
+    end
+  end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,34 @@
 require "test_helper"
 
 describe UsersController do
+  before do
+    @user = users(:tram)
+  end
   it "can get login_form" do
     get login_path
 
     must_respond_with :success
+  end
+
+  it "must get index" do
+    get users_path
+
+    must_respond_with :success
+
+
+  end
+
+  it "must get show" do
+    get user_path(@user.id)
+    must_respond_with :success
+
+  end
+
+  it "will give a 404 error for a nonexistence user" do
+
+    get user_path(-1)
+
+    must_respond_with :not_found
   end
 
   describe "logging in" do
@@ -27,6 +51,23 @@ describe UsersController do
       expect(user.username).must_equal user_hash[:user][:username]
     end
   end
+
+  describe "logout" do
+    
+  end
+
+  # describe "current" do
+  #   it "returns 200 OK for a logged-in user " do
+  #     # Arrange
+  #     perform_login
+  #
+  #     # Act
+  #     get current_user_path
+  #
+  #     # Assert
+  #     must_respond_with :success
+  #   end
+  # end
 
 
 end
