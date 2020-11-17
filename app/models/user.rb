2beae91ef1f5cccb9bc0056080864dbcc25c93d1
Tram Bui <pbui17@cmc.edu>
2020-11-17 10:34:52 -0600

    added migration to add fields to the user table and created helper method in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
 
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = auth_hash["provider"]
+    user.username = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+
+    # note that the user has not been saved
+    # we'll choose to do the saving outside of this method
+    return user
+  end
+
 end
