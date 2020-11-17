e0e1bf1d42f2744200a533daa39413d8f7434791
Tram Bui <pbui17@cmc.edu>
2020-11-17 10:46:54 -0600

    commented out superfluous code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,26 +44,28 @@ class UsersController < ApplicationController
     return redirect_to root_path
   end
 
-  def login_form
-    @user = User.new
-  end
-
-  def login
-    username = params[:user][:username]
-    user = User.find_by(username: username)
-
-    if user # existing user
-      session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as existing user #{username}"
-    else # new user "Successfully created new user testingcat with ID 874"
-      user = User.create(username: username)
-      session[:user_id] = user.id
-      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
-    end
-
-    redirect_to root_path
-    return
-  end
+  # TODO: delete because we don't need a generic login_form
+  # and login action anymore w/OAuth
+  # def login_form
+  #   @user = User.new
+  # end
+  #
+  # def login
+  #   username = params[:user][:username]
+  #   user = User.find_by(username: username)
+  #
+  #   if user # existing user
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Successfully logged in as existing user #{username}"
+  #   else # new user "Successfully created new user testingcat with ID 874"
+  #     user = User.create(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+  #   end
+  #
+  #   redirect_to root_path
+  #   return
+  # end
 
   def logout
     session[:user_id] = nil
